81ffaee5e88c943c54131b98f90d9b8076593fdc
Kalaiselvan_1503 <30726230+Kalaiselvan1503@users.noreply.github.com>
2019-01-07 14:45:49 +0530

    Create greatest3.py

---
+++ greatest3.py
@@ -0,0 +1,15 @@
+a=int(input())
+b=int(input())
+c=int(input())
+if a>b and a>c:
+    print(a)
+elif b>a and b>c:
+    print(b)
+elif c>a and c>b:
+    print(c)
+elif a==b or b==a:
+    print(a)
+elif b==c or c==b:
+    print(b)
+else:
+    print(c)
