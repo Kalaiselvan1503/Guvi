c0caa62310d64a0d006d666c5313afbaff68af06
Kalaiselvan_1503 <30726230+Kalaiselvan1503@users.noreply.github.com>
2019-01-08 22:09:28 +0530

    Create arms_range.py

---
+++ arms_range.py
@@ -0,0 +1,19 @@
+#1234
+l,u=input().split()
+l=int(l)
+u=int(u)
+m=[]
+for num in range(l, u + 1):
+   order = len(str(num))
+   sum = 0
+   temp = num
+   while temp > 0:
+       digit = temp % 10
+       sum += digit ** order
+       temp //= 10
+   if num == sum:
+       m.append(num)
+       print(m)
+for k in range(0,len(m)-1):
+  print(m[k],end=" ")
+print(m[len(m)-1])
