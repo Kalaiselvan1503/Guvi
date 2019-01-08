7fb117d6dd205f3cbaf0e5496abeccaca7f383ff
Kalaiselvan_1503 <30726230+Kalaiselvan1503@users.noreply.github.com>
2019-01-08 19:37:30 +0530

    Create Array_sum.java

---
+++ Array_sum.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Array_sum{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int k=in.nextInt();
+		int sum=0;
+		int a[]=new int[n];
+		for (int i = 0; i < a.length; i++) {
+			a[i]=sc.nextInt();
+		}
+		for(int i=0;i<k;i++)
+		{
+			sum=sum+a[i];
+		}
+			System.out.println(sum);		
+	}
+}
