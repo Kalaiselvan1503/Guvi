63b5cc197369d21a547510f4efb7a887c7c7d1a0
Kalaiselvan_1503 <30726230+Kalaiselvan1503@users.noreply.github.com>
2019-01-08 21:49:44 +0530

    Create Prime_from.java

---
+++ Prime_from.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class Prime_from{
+    public static void main(String[] args) {
+          Scanner a=new Scanner(System.in);
+          int n=a.nextInt();
+          int q=a.nextInt();
+          int f,i;
+          while(n<q)
+                  {
+                      f=0;
+                      for(i=2;i<=n/2;i++)
+                      {
+                          if(n%i==0)
+                          {
+                              f=1;
+                              break;
+                          }
+                      }
+                      if(f==0)
+                      {
+                          if(n==1)
+                          {
+                              
+                          }
+                          else
+                          System.out.println(n);
+                      }
+                      ++n;
+                      
+                  }
+    }
+    }
