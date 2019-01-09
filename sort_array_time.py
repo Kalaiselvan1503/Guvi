396e06e61c2f09bd8c2f89a0118e58b26033bf72
Kalaiselvan_1503 <30726230+Kalaiselvan1503@users.noreply.github.com>
2019-01-09 16:01:50 +0530

    Update sort_array_time.py

--- sort_array_time.py
+++ sort_array_time.py
@@ -1,7 +1,7 @@
 #1234
-h=int(input())
-n=list(map(int,input().split()))
-s=sorted(h)
+f=int(input())
+d=list(map(int,input().split()))
+s=sorted(d)
 for i in range(0,len(s)):
     if i==0:
         print(s[i],end="")
