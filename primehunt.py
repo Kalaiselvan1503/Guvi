1f2d421f98afefe4b63bce2960459f091bf7275a
Kalaiselvan_1503 <30726230+Kalaiselvan1503@users.noreply.github.com>
2019-02-14 15:55:21 +0530

    Create primehunt.py

---
+++ primehunt.py
@@ -0,0 +1,33 @@
+#hsdjg
+import java.util.*;
+class Prime1{
+    public static void main(String[] args) {
+          Scanner a=new Scanner(System.in);
+          int n=1;
+          int q=a.nextInt();
+          int f,i;
+          while(n<q)
+                  {
+                      f=0;
+                      for(i=2;i<=n/2;i++)
+                      {
+                          if(n%i==0)
+                          {
+                              f=1;
+                              break;
+                          }
+                      }
+                      if(f==0)
+                      {
+                          if(n==1)
+                          {
+                              
+                          }
+                          else
+                          System.out.print(n+" ");
+                      }
+                      ++n;
+                      
+                  }
+    }
+    }
